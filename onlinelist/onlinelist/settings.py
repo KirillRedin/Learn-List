3b1eeb9ccaf05d44c779ca9b5762284fc1f846d9
Kirill Redin <redinkirill.ua@gmail.com>
2018-05-19 19:15:41 +0300

    Created models for db

--- onlinelist/onlinelist/settings.py
+++ onlinelist/onlinelist/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playlist',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'onlinelist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learnlistdb',
+        'USER': 'root',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
--- onlinelist/playlist/models.py
+++ onlinelist/playlist/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    data = models.ForeignKey('Data', models.DO_NOTHING)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+    content = models.TextField()
+
+    class Meta:
+        db_table = 'comment'
+
+
+class Data(models.Model):
+    id = models.AutoField(primary_key=True)
+    playlist = models.ForeignKey('Playlist', models.DO_NOTHING)
+    link = models.CharField(max_length=200)
+    picture = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'data'
+        unique_together = (('id', 'playlist'),)
+
+
+class Playlist(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+    description = models.TextField(blank=True, null=True)
+    picture = models.CharField(max_length=200, blank=True, null=True)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'playlist'
+
+
+class Privilege(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('User', models.DO_NOTHING)
+    playlist = models.ForeignKey(Playlist, models.DO_NOTHING)
+    accessNum = models.IntegerField()
+
+    class Meta:
+        db_table = 'privilege'
+        unique_together = (('id', 'user', 'playlist'),)
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    login = models.CharField(max_length=45)
+    status = models.IntegerField()
+    picture = models.CharField(max_length=200)
+    email = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        db_table = 'user'
